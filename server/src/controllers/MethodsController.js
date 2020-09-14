2b64c766cdc5781fd419b727d4151731daf25fd6
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2020-09-14 16:35:52 -0300

    Adiciona controller de metodos

---
+++ server/src/controllers/MethodsController.js
@@ -0,0 +1,31 @@
+const db = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const metodos = await db('metodos')
+                            .select('*');
+
+        return response.json( metodos );
+    },
+
+    async create(request, response) {
+        const { nome, descricao } = request.body;
+
+        await db('metodos').insert({
+            nome, 
+            descricao
+        })
+    
+        return response.status(201).send();
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await dbConnection('metodos')
+            .where('id', id)
+            .delete(); 
+
+        return response.status(204).send();
+    }
+}
